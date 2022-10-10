bb69a47d61e27fab820ca966a7e844039a2e1981
rafael <rafaelbordoni00@gmail.com>
2022-10-10 14:58:17 -0300

    chore: update put /entries integration tests

--- tests/integration/updateEntry.test.ts
+++ tests/integration/updateEntry.test.ts
@@ -40,4 +40,28 @@ describe('PUT /entries integration tests...', () => {
     expect(query?.sourceUrl).toBe(newData.sourceUrl);
     expect(query?.space).toBe(newData.space);
   });
+
+  it('Sending to wrong page, should return 401 and not change anything', async () => {
+    await createUser(userFactory());
+    await createUser(userFactory());
+    const token = generateToken(1);
+    const page = pageFactory();
+    await db.page.create({ data: { ...page, userId: 2 } });
+    const entryData = entryDataFactory();
+    await db.entry.create({ data: { ...entryData, pageId: 1, index: 0 } });
+    const newData = entryDataFactory();
+    const response = await supertest(app)
+      .put(`/entries/1/1`)
+      .set({ Authorization: `Bearer ${token}` })
+      .send(newData);
+    const query = await db.entry.findUnique({ where: { id: 1 } });
+    expect(response.status).toBe(401);
+    expect(query?.type).toBe(entryData.type);
+    expect(query?.title).toBe(entryData.title);
+    expect(query?.description).toBe(entryData.description);
+    expect(query?.text).toBe(entryData.text);
+    expect(query?.imageUrl).toBe(entryData.imageUrl);
+    expect(query?.sourceUrl).toBe(entryData.sourceUrl);
+    expect(query?.space).toBe(entryData.space);
+  });
 });
